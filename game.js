f41217513518852fb576b3a06f55cd3ff615c392
RainLin05 <145607177+RainLin05@users.noreply.github.com>
2023-10-30 15:32:13 -0400

    Update game.js

---
+++ game.js
@@ -0,0 +1,17 @@
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    while (currentIndex > 0) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
+  var arr = [1, 2, 3 , 4, 5, 6, 7, 8];
+  shuffle(arr);
