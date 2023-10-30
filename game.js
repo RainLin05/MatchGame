ed943d7849001a7655e1afc17f3c7aedd7f0b7ee
MDSinta <106995098+MDSinta@users.noreply.github.com>
2023-10-30 15:35:28 -0400

    Update game.js

--- game.js
+++ game.js
@@ -15,3 +15,7 @@ function shuffle(array) {
   
   var arr = [1, 2, 3 , 4, 5, 6, 7, 8];
   shuffle(arr);
+let changeImg = () => {
+    console.log("aaaaaa")
+    document.getElementById("1").querySelector("img").src="bird.jpg";
+}
