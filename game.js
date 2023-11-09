c425a8481665782f6f4a1601e6ba85c449f844dd
rainlin05 <rainl5@nycstudents.net>
2023-11-08 22:55:23 -0500

    birb2

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var birds = ["birb0.jpg","birb1.jpg","birb2.jpg","birb3.jpg","birb4.jpg","birb5.jpg","birb6.jpg","birb7.jpg","bird8.jpeg","birb0.jpg","birb1.jpg","birb2.jpg","birb3.jpg","birb4.jpg","birb5.jpg","birb6.jpg","birb7.jpg","bird8.jpeg"];
+var birds = ["birb0.jpg","birb1.jpg","birb2.jpg","birb3.jpg","birb4.jpg","birb5.jpg","birb6.jpg","birb7.jpg","bird8.jpg","birb0.jpg","birb1.jpg","birb2.jpg","birb3.jpg","birb4.jpg","birb5.jpg","birb6.jpg","birb7.jpg","bird8.jpg"];
 let count = 0;
 let score = 0;
 var newBirds = shuffle(birds);
